558dc164a9367c1c89343b9ea0d4da86408784a9
marcusjoe-hub <marcusjoe.k@gmail.com>
2026-09-10 22:49:59 +0530

    Update gallery-data.js

--- js/gallery-data.js
+++ js/gallery-data.js
@@ -1,5 +1,7 @@
 // Add your image paths here. Upload images to assets/img/ on GitHub.
 // Maximum 9 images will display. If you add 10, the oldest (top) is hidden.
 export const galleryImages = [
-    // Example: "assets/img/screenshot1.png",
+    "assets/img/screenshot1.png",
+    "assets/img/screenshot2.png",
+    "assets/img/screenshot3.png",
 ];
